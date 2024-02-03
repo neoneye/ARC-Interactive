0d98f8913665d992c583351b0429198857eedaa4
Simon Strandgaard <neoneye@gmail.com>
2024-02-03 23:35:59 +0100

    Roman fresco color palette.

--- webroot/common.js
+++ webroot/common.js
@@ -272,7 +272,7 @@ class Theme {
     }
 
     static getAvailableThemes() {
-        return ["default", "paultolmuted", "paultolsequential", "bluecyanwhite", "greyscale", "c64", "secam", "ega", "amigaworkbench13", "applerainbow"];
+        return ["default", "paultolmuted", "paultolsequential", "bluecyanwhite", "greyscale", "c64", "secam", "ega", "amigaworkbench13", "applerainbow", "romanfresco"];
     }
 
     // Assign the theme to the <body> element.
