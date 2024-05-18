cd35ce00eee895c1f6f816d68eb5fe7854938c11
Simon Strandgaard <neoneye@gmail.com>
2024-05-18 08:59:13 +0200

    Reworked mechanism for hiding buggy tasks, so it now also includes ARC + ConceptARC.

--- webroot/meta-issue.js
+++ webroot/meta-issue.js
@@ -1,32 +1,51 @@
-const META_ARC_ISSUE = {
-    "423a55dc": {
-        "url": "https://github.com/fchollet/ARC/issues/98",
-        "title": "Test is unsolvable, skew.",
-        "fix": "423a55dc_v2",
+const META_ISSUE = {
+    "ARC": {
+        "423a55dc": {
+            "url": "https://github.com/fchollet/ARC/issues/98",
+            "title": "Test is unsolvable, skew.",
+            "fix": "423a55dc_v2",
+        },
+        "58e15b12": {
+            "url": "https://github.com/fchollet/ARC/issues/86",
+            "title": "Test is unsolvable, when green and aqua lines are intersect they should produce magenta color. But in the provided test output single magenta cell is missing.",
+            "fix": "58e15b12_v2",
+        },
+        "a8610ef7": {
+            "url": "https://github.com/fchollet/ARC/issues/89",
+            "title": "Test is unsolvable, all cyan cells should be grey.",
+            "fix": "a8610ef7_v2",
+        },
+        "b4a43f3b": {
+            "url": "https://github.com/fchollet/ARC/issues/101",
+            "title": "Test is unsolvable, missing 2 green pixels.",
+            "fix": "b4a43f3c_v2",
+        },
+        "bd14c3bf": {
+            "url": "https://github.com/fchollet/ARC/issues/73",
+            "title": "Test is solvable. Minor inconsistency. Input top left object should be red.",
+            "fix": "bd14c3bf_v2",
+        },
+        "dc433765": {
+            "url": "https://github.com/fchollet/ARC/issues/29",
+            "title": "Test is solvable. Minor inconsistency. Redundant train and test.",
+            "fix": "dc433765_v2",
+        }
     },
-    "58e15b12": {
-        "url": "https://github.com/fchollet/ARC/issues/86",
-        "title": "Test is unsolvable, when green and aqua lines are intersect they should produce magenta color. But in the provided test output single magenta cell is missing.",
-        "fix": "58e15b12_v2",
-    },
-    "a8610ef7": {
-        "url": "https://github.com/fchollet/ARC/issues/89",
-        "title": "Test is unsolvable, all cyan cells should be grey.",
-        "fix": "a8610ef7_v2",
-    },
-    "b4a43f3b": {
-        "url": "https://github.com/fchollet/ARC/issues/101",
-        "title": "Test is unsolvable, missing 2 green pixels.",
-        "fix": "b4a43f3c_v2",
-    },
-    "bd14c3bf": {
-        "url": "https://github.com/fchollet/ARC/issues/73",
-        "title": "Test is solvable. Minor inconsistency. Input top left object should be red.",
-        "fix": "bd14c3bf_v2",
-    },
-    "dc433765": {
-        "url": "https://github.com/fchollet/ARC/issues/29",
-        "title": "Test is solvable. Minor inconsistency. Redundant train and test.",
-        "fix": "dc433765_v2",
+    "ConceptARC": {
+        "AboveBelow3": {
+            "url": "https://github.com/victorvikram/ConceptARC/issues/4",
+            "title": "Test is solvable. Minor inconsistency, size mismatch.",
+            "fix": "AboveBelow3_v2",
+        },
+        "InsideOutside8": {
+            "url": "https://github.com/victorvikram/ConceptARC/issues/3",
+            "title": "Test is solvable. Minor inconsistency, size mismatch.",
+            "fix": "InsideOutside8_v2",
+        },
+        "InsideOutsideMinimal": {
+            "url": "https://github.com/victorvikram/ConceptARC/commit/67befab8e44c902327502242f524e3f4af35523a",
+            "title": "Test is solvable. Minor inconsistency, missing pixel.",
+            "fix": "InsideOutsideMinimal_v2",
+        }
     }
 };
